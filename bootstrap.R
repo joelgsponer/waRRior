2141a4ccde41ad8a00c816581d733ee9550d2c56
joelgsponer <jgsponer@gmail.com>
2015-04-24 15:57:59 +0200

    Update bootstrap.R

--- bootstrap.R
+++ bootstrap.R
@@ -1,1 +1,15 @@
+source_https <- function(url, ...) {
+  # load package
+  require(RCurl)
+ 
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
+sink("~/.waRRior-log")
+source_https(
+  "https://raw.githubusercontent.com/joelgsponer/waRRior/master/github/read/data/waRRior.github.read.data.R"
+)
+sink()
 
