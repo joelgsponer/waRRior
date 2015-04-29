0c634ddb3b261456451b91e0311380ab937e2532
joelgsponer <jgsponer@gmail.com>
2015-04-29 11:11:27 +0200

    Update waRRior.google.colors.accents.R

--- google/colors/waRRior.google.colors.accents.R
+++ google/colors/waRRior.google.colors.accents.R
@@ -5,7 +5,10 @@ waRRior.google.colors.accents <- function(level = 4, shuffle = F, show = F){
 	  	r <- c(r, google.colors[[i]]$accent[level])
 	  }
 	  if(shuffle) r <- sample(r)
-	  if(show)par(mar = c(4,8,4,4));barplot(rep(1,16), col = r, names = paste(seq(1,16), r), las = 2, horiz = T, xaxt = "n")
+	  if(show){
+	    par(mar = c(4,8,4,4))
+	    barplot(rep(1,16), col = r, names = paste(seq(1,16), r), las = 2, horiz = T, xaxt = "n")
+	  }
 	  return(r)
 	}else{
 	  stop("google colors have not been loaded.")
