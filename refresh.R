1734cfc64cc38832d51458dd8bf5851400e61347
joelgsponer <jgsponer@gmail.com>
2015-04-29 10:48:01 +0200

    Update refresh.R

--- refresh.R
+++ refresh.R
@@ -1,7 +1,7 @@
 waRRior.refresh <- function(verbose = T){
-  if(verbose)cat("waRRior: refreshing.")
-  if(verbose)cat("waRRior: cleaning up.")
+  if(verbose)cat("waRRior: refreshing.\n")
+  if(verbose)cat("waRRior: cleaning up.\n")
   rm(ls()[grep("waRRior", ls())])
   waRRior.github.read.code("https://raw.githubusercontent.com/joelgsponer/waRRior/master/bootstrap.R")
-  if(verbose)cat("waRRior: done refreshing.")
+  if(verbose)cat("waRRior: done refreshing.\n")
 }
