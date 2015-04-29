37d51f121f91a31949f30b4d05394bf593dc6819
joelgsponer <jgsponer@gmail.com>
2015-04-29 10:47:11 +0200

    Update bootsrap.local.R

--- bootsrap.local.R
+++ bootsrap.local.R
@@ -1,13 +1,15 @@
 #Replace the github Version of waRRior with a local one to ensure updates do not break a working project
-waRRior.bootstrap.local <- function(path="waRRior/"){
-  cat("
+waRRior.bootstrap.local <- function(path="waRRior/", verbose = T){
+  if(verbose)cat("
     ***********************************
     ** local waRRior is being loaded **
-    ***********************************
+    ***********************************\n
   ")
 
-  cat("waRRior: cleaning the workspace.")
-  rm(grep("waRRior.*",ls()))
-
+  if(verbose)cat("waRRior: cleaning the workspace.\n")
+  rm(ls()[grep("waRRior", ls())])
+  
+  if(verbose)cat("waRRior: loading local files in path",path,".\n")
   file.sources = list.files(pattern="waRRior/*.R")
   sapply(file.sources,source,.GlobalEnv)
+}
