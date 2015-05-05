88beb6de34dbf5a5ddb70c3e38ed2e940f22ff6f
Joel Gsponer <jgsponer@gmail.com>
2015-05-05 12:10:20 +0200

    Update waRRior.snippets.backup_file.R

--- snippets/waRRior.snippets.backup_file.R
+++ snippets/waRRior.snippets.backup_file.R
@@ -8,6 +8,7 @@ waRRior.snippets.backup_file <- function(
   ,is.silent = F #it T supresses raising of errors, istead return a list with error information.
   ,function.id = "waRRior.snippets.backup_file" #Use this to identfy the function in error (or success messages if applicable) messages.
   ,verbose = F #Turn messages on and off
+  ,debug = F
   ,...){
   tryCatch({
      
@@ -16,19 +17,34 @@ waRRior.snippets.backup_file <- function(
     files.in.backup.location <- sort(list.files(destination, pattern=identifier))
     if(verbose)cat("waRRior: files in backup location matching the identifier\n")
     if(verbose){
-      for(file in files.in.backup.location) cat("\t*",file,"\n")
+      for(f in files.in.backup.location) cat("\t*",f,"\n")
     }
     nr.files.in.backup.location <- length(files.in.backup.location)
     if(verbose)cat("waRRior: total number of files", nr.files.in.backup.location,"\n")
     
     while(nr.files.in.backup.location >= max.backup.files){
       if(verbose)cat("waRRior: deleting previous backup file",files.in.backup.location[1],"due to max.backup.files restriction (max files:",max.backup.files,"\n")
-      file.remove(paste(destination,files.in.backup.location[1],sep =""))
+      res.file.remove <- file.remove(paste(destination,files.in.backup.location[1],sep ="/"))
+      if(debug)print(res.file.remove)
+      if(!(res.file.remove))stop("Error while deleting files")
+      files.in.backup.location <- sort(list.files(destination, pattern=identifier))
+      nr.files.in.backup.location <- length(files.in.backup.location)
     }
-    backup.file.name <- paste(Sys.time,identifier,sep = "_")
+    
+    backup.file.name <- gsub(" ","_",paste(Sys.time(),identifier,sep = "_"))
+    backup.file.name <- gsub(":","_",backup.file.name)
+    if(debug)print(backup.file.name)
+    if(debug)print(file)
+    backup.file.ending <- unlist(strsplit(file,"[.]"))[length(unlist(strsplit(file,"[.]")))]
+    if(debug)print(backup.file.ending)
+    backup.file.name <- paste(backup.file.name,backup.file.ending,sep = ".")
+    if(debug)print(backup.file.name)
+    
     backup.file.path <- paste(destination, backup.file.name, sep = "/")
-    if(verbose)cat("waRRior: creating backup",backup.file.name,"\n")
-    file.copy(file, backup.file.path)
+    if(verbose)cat("waRRior: creating backup",backup.file.path,"\n")
+    res.file.copy <- file.copy(file, backup.file.path)
+    if(debug)print(res.file.copy)
+    if(!(res.file.copy))stop("Error while copying files")
     
     #define response
     response <- T
