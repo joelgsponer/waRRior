b083ba7b3bcf29e71600e074c2d1c27b97ab43bf
Joel Gsponer <jgsponer@gmail.com>
2015-05-06 16:22:39 +0200

    Update refresh.R

--- refresh.R
+++ refresh.R
@@ -1,7 +1,7 @@
 waRRior.refresh <- function(verbose = T){
-  waRRior.snippets.verbose('refreshing.')
-  waRRior.snippets.verbose('cleaning up.')
+  waRRior.snippets.verbose('refreshing.', verbose_ = verbose)
+  waRRior.snippets.verbose('cleaning up.', verbose_ = verbose)
   for(i in grep("waRRior", ls(),value = T)) rm(i)
   waRRior.github.read.code("https://raw.githubusercontent.com/joelgsponer/waRRior/master/bootstrap.R")
-  waRRior.snippets.verbose('waRRior: done refreshing.')
+  waRRior.snippets.verbose('waRRior: done refreshing.', verbose_ = verbose)
 }
