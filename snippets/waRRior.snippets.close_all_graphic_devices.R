675b0772b502ec544354e329701d25f3dbf6dd3e
Joel Gsponer <jgsponer@gmail.com>
2015-05-06 10:55:56 +0200

    Update waRRior.snippets.close_all_graphic_devices.R

--- snippets/waRRior.snippets.close_all_graphic_devices.R
+++ snippets/waRRior.snippets.close_all_graphic_devices.R
@@ -2,6 +2,6 @@ waRRior.snippets.close_all_graphic_devices <- function(verbose = F){
   tryCatch({
       while(T) dev.off()
     },error = function(e){
-      waRRior.snippets.verbose('closed all graphic devices.')
+      waRRior.snippets.verbose('closed all graphic devices.', verbose_ = verbose)
     })
 }
