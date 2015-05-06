5a1a4b65e1964821e420ea34a89e74524d42a3d8
Joel Gsponer <jgsponer@gmail.com>
2015-05-06 17:58:12 +0200

    Update waRRior.bioinformatics.genomics.display_cytoband.R

--- bioinformatics/genomics/waRRior.bioinformatics.genomics.display_cytoband.R
+++ bioinformatics/genomics/waRRior.bioinformatics.genomics.display_cytoband.R
@@ -6,6 +6,7 @@ waRRior.bioinformatics.genomics.display_cytoband <- function(
   ,color.border = "black"
   ,y.ubound = 1
   ,y.lbound = -1
+  ,y.text = -4
   ,debug = F
 ){
   df <- waRRior.github.read.data("https://raw.githubusercontent.com/joelgsponer/annotate-it/master/bioinformatics/genomics/cytobandshg19.txt",do.dplyr = F,  sep = " ")
@@ -13,8 +14,8 @@ waRRior.bioinformatics.genomics.display_cytoband <- function(
   df <- df[as.numeric(as.character(df$chrnumeric)) == chr,]
   if(debug)print(df)
   if(create.plot){
-    plot(1,1, xlim = range(df[,c("start", "end")]), pch = NA, ylim = c(-5,5), ylab = NA, las = 2,xaxt = "n", yaxt = "n", xlab = NA)
-    axis(1, apply(df[,c("start", "end")], 1, mean), df$band, las = 2, cex.axis = 0.8)
+    plot(1,1, xlim = range(df[,c("start", "end")]), pch = NA, ylim = c(-5,5), ylab = NA, las = 2,xaxt = "n", yaxt = "n", xlab = NA, bty = "n")
+    text(apply(df[,c("start", "end")], 1, mean),c(y.text), df$band, las = 2, cex = 0.8,srt = 90 )
   }    
   color.bands <- sapply(df$stain, FUN = function(x){waRRior.snippets.make_color_transparent(color.bands, alpha = x)})
   if(debug)print(color.bands)
