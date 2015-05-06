b5fc3ba1df8bb30a7dcf2a3eccbc3e4e16c5217c
Joel Gsponer <jgsponer@gmail.com>
2015-05-06 10:13:00 +0200

    Update bootstrap.R

--- bootstrap.R
+++ bootstrap.R
@@ -28,6 +28,8 @@ files.to.be.loaded.on.startup <- list(
   #Bioinformatics
   ##aCGH
   ,"https://raw.githubusercontent.com/joelgsponer/waRRior/master/bioinformatics/acgh/waRRior.bioinformatics.acgh.nowaves.R"
+  ##Flowcytometry
+  ,"https://raw.githubusercontent.com/joelgsponer/waRRior/master/bioinformatics/flowcytomety/waRRior.bioinformatics.flowcytometry.color_cell_cycle.R"
   #Snippets
   ,"https://raw.githubusercontent.com/joelgsponer/waRRior/master/snippets/waRRior.snippets.close_all_graphic_devices.R"
   ,"https://raw.githubusercontent.com/joelgsponer/waRRior/master/snippets/waRRior.snippets.backup_file.R"
