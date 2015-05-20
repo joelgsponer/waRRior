fe563d09b0b12e8efd1189c2204e4605b55909e0
Joel Gsponer <jgsponer@gmail.com>
2015-05-20 21:58:22 +0200

    Update waRRior.machinelearning.geneticalgorithm.crossover.R

--- machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.crossover.R
+++ machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.crossover.R
@@ -3,9 +3,9 @@ waRRior.machinelearning.geneticalgorithm.crossover <- function(
    ...
   ,is.silent = F #it T supresses raising of errors, istead return a list with error information.
   ,function.id = 'waRRior.machinelearning.genetic_algorithm.crossover' #Use this to identfy the function in error (or success messages if applicable) messages.
-  ,verbose = T #Turn messages on and off
+  ,verbose = F #Turn messages on and off
   ,simpleReturn = T
-  ,debug = T #Turn debug messages on and off
+  ,debug = F #Turn debug messages on and off
 ){
   tryCatch({
     t <- Sys.time() 
