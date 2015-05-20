d2ab04ac5b8c548cdd75164cb6c6a442790a5297
Joel Gsponer <jgsponer@gmail.com>
2015-05-20 19:56:38 +0200

    Update waRRior.snippets.create_random_string.R

--- snippets/waRRior.snippets.create_random_string.R
+++ snippets/waRRior.snippets.create_random_string.R
@@ -1,3 +1,3 @@
 waRRior.snippets.create_random_string <- function(n){
-  return(paste(sample(c(0:9, letters, LETTERS), 10, replace = T),collapse = ""))
+  return(paste(sample(c(0:9, letters, LETTERS), n, replace = T),collapse = ""))
 }
