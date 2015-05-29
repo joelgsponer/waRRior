7bf000040bc0c122b9806c1448e806214f7b1398
Joel Gsponer <jgsponer@gmail.com>
2015-05-29 15:11:58 +0200

    Update waRRior.snippets.verbose.R

--- snippets/waRRior.snippets.verbose.R
+++ snippets/waRRior.snippets.verbose.R
@@ -1,5 +1,5 @@
 waRRior.snippets.verbose <- function(message,verbose_ = T){
   if(verbose_){
-    cat(format(Sys.time()),"-> waRRior:",message,"\n")
+    cat("[",format(Sys.time()),"] waRRior: ",message,"\n", sep = "")
   }
 }
