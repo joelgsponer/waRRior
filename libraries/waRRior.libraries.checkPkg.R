fed59d226b4fc2731d46ce5fcc01f7fb32f79bbc
Joel Gsponer <jgsponer@gmail.com>
2015-05-29 14:10:49 +0200

    Update waRRior.libraries.checkPkg.R

--- libraries/waRRior.libraries.checkPkg.R
+++ libraries/waRRior.libraries.checkPkg.R
@@ -1,7 +1,7 @@
 waRRior.libraries.checkPkg <- function(pkg){
   if(!is.element(pkg, installed.packages()[,1]))
     {
-   waRRior.snippets.verbose("!",pkg,"not found - trying to install", verbose_ = T)
+   waRRior.snippets.verbose(paste("!",pkg,"not found - trying to install"), verbose_ = T)
    install.packages(pkg, repos="http://cran.us.r-project.org")
-  }else {waRRior.snippets.verbose("\t*",pkg,"library already installed\n", verbose_=T)}
+  }else {waRRior.snippets.verbose(paste("\t*",pkg,"library already installed\n"), verbose_=T)}
 }
