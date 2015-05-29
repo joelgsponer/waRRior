f860c1286fb420d9045bacdbc95c3da75db7fec8
Joel Gsponer <jgsponer@gmail.com>
2015-05-29 15:08:25 +0200

    Update waRRior.google.colors.load.R

--- google/colors/waRRior.google.colors.load.R
+++ google/colors/waRRior.google.colors.load.R
@@ -1,10 +1,10 @@
 waRRior.google.colors.load <- function(
   url = "https://raw.githubusercontent.com/joelgsponer/waRRior/master/google/colors/waRRior.google.colors.json"
 ){
-  cat("waRRior: Loading google.colors.\n")
+  waRRior.snippets.verbose("loading google.colors.\n")
   google.colors <- fromJSON(getURL(url))
   assign("google.colors", google.colors, envir = .GlobalEnv)
-  cat("waRRior: google.colors loaded.\n")
+  waRRior.snippets.verbose("google.colors loaded.\n")
 }
 
 waRRior.google.colors.load()
