2345a04436b41b15bab7469e3fa325609b727432
Joel Gsponer <jgsponer@gmail.com>
2015-06-02 21:41:04 +0200

    Update waRRior.machinelearning.geneticalgorithm.crossover.R
    added support for boolean_vector

--- machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.crossover.R
+++ machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.crossover.R
@@ -3,6 +3,7 @@ waRRior.machinelearning.geneticalgorithm.crossover <- function(
    ...
   ,is.silent = F #it T supresses raising of errors, istead return a list with error information.
   ,function.id = 'waRRior.machinelearning.genetic_algorithm.crossover' #Use this to identfy the function in error (or success messages if applicable) messages.
+  ,genes.class = genes.class
   ,verbose = F #Turn messages on and off
   ,simpleReturn = T
   ,debug = F #Turn debug messages on and off
@@ -11,13 +12,23 @@ waRRior.machinelearning.geneticalgorithm.crossover <- function(
     t <- Sys.time() 
     #Actual Function code
     chrs <- list(...)
-
-    
     nr_of_chrs <- length(chrs)
     waRRior.snippets.verbose(paste('nr of chromosomes:', nr_of_chrs), verbose_ = verbose)
 
     nr_of_genes <- length(chrs[[1]])
     
+    for(i in seq(1,nr_of_chrs)){
+     chrs[[i]] <- lapply(chrs[[i]]
+                  ,FUN=function(x){
+                    if(length(x) > 1){ 
+                      return(paste(x, collapse = ";"))
+                    }else{
+                     return(x)
+                    }
+                  }
+                )
+    }
+    if(debug)print(chrs)
     mat <- matrix(unlist(chrs),nr_of_genes, nr_of_chrs)
     colnames(mat) <- names(chrs)
     rownames(mat) <- names(chrs[[1]])
@@ -32,12 +43,20 @@ waRRior.machinelearning.geneticalgorithm.crossover <- function(
     
     
     new_chr <- apply(mat, 1, FUN = sample, size = 1) 
+    new_chr <- as.list(new_chr)
+    new_chr <- lapply(new_chr, FUN=function(x){
+              if(grepl(";",x)){ 
+                return(unlist(strsplit(x,";")))
+              }else{
+                return(x)
+              }
+             })
     if(debug) print(new_chr)
         
     #define response
-    if(simpleReturn) return(as.list(new_chr))
+    if(simpleReturn) return(new_chr)
     response <- list(
-       result <- as.list(new_chr)
+       result <- new_chr
       ,message <- paste(function.id, 'success')
       ,error = 0
       ,e = NULL
