6bf4c5a8af4291e6955542f80734354057283438
Joel Gsponer <jgsponer@gmail.com>
2015-06-08 14:19:29 +0200

    Update ga_gbm.R

--- machinelearning/h2o/ga_gbm.R
+++ machinelearning/h2o/ga_gbm.R
@@ -1,81 +1,85 @@
 
-response = coretable.cleaned.vanilla$variables.output[2]
-predictors = coretable.cleaned.vanilla$variables.input
+waRRior.machinelearning.h2o.ga_gbm <- function(
+   response
+  ,predictors
+  ,feature.selection = T
+  ,...
+  ){
 
-evaluate_gbm <- function(
-   object
-  ,train_hex
-  ,valid_hex
-  ,test_hex
-  ,response_ = response
-  ,predictors_ = predictors
-){
-  #Function code
-  model <- h2o.gbm(
-     x = predictors_[object@chr$variables.include]
-    ,y = response_
-    ,data = train_hex
-    ,distribution = "multinomial"
-    ,n.trees = object@chr$n.trees
-    ,interaction.depth = object@chr$interaction.depth
+  evaluate_gbm <- function(
+     object
+    ,train_hex
+    ,valid_hex
+    ,test_hex
+    ,response_ = response
+    ,predictors_ = predictors
+  ){
+    #Function code
+    model <- h2o.gbm(
+       x = predictors_[as.logical(object@chr[seq(1,length(predictors_))])]
+      ,y = response_
+      ,data = train_hex
+      ,distribution = object@chr$distribution
+      ,n.trees = as.numeric(object@chr$n.trees)
+      ,interaction.depth = as.numeric(object@chr$interaction.depth)
+      ,n.minobsinnode = as.numeric(object@chr$n.minobsinnode)
+      ,shrinkage = as.numeric(object@chr$shrinkage)
+      ,n.bins = as.numeric(object@chr$n.bins)
+      ,importance = F
+      #,nfolds = 2
+      ,validation = valid_hex
+      #,balance.classes = F
+      ,max.after.balance.size = as.numeric(object@chr$max.after.balance.size)
+    )
+    score <- h2o.get_auc(model, test_hex, response = response_, debug = F)
+    #Retrun Score
+    return(score)
+  }
+
+  if(feature.selection)chr.predictors <- as.list(sample(c(F,T), length(predictors), replace = T))
+  chr <- list(
+     n.trees = 5
+    ,interaction.depth = 5
     ,n.minobsinnode = 10
-    ,shrinkage = 0.1
     ,n.bins = 20
-    ,importance = F
-    #, nfolds = 2
-    ,validation = valid_hex
-    ,balance.classes = FALSE
     ,max.after.balance.size = 5
+    ,distribution = "multinomial"
+    ,shrinkage = 0.1
   )
-  score <- h2o.get_auc(model, test_hex, response = response_)
-  #Retrun Score
-  return(score)
+  if(feature.selection) chr <- c(chr.predictors, chr)
+
+  if(feature.selection)genes.class.predictors <- as.list(rep("boolean", length(predictors)))
+  genes.class <- list(
+     "integer"
+    ,"integer"
+    ,"integer"
+    ,"integer"
+    ,"integer"
+    ,"class"
+    ,"float"
+  )
+  if(feature.selection)genes.class <- c(genes.class.predictors, genes.class)
+
+  if(feature.selection)genes.range.predictors <- as.list(rep(T, length(predictors)))
+  genes.range <- list(
+    c(1,100)
+   ,c(1,100)
+   ,c(1,100)
+   ,c(2,1000)
+   ,c(1,100)
+   ,c("multinomial","gaussian")
+   ,c(0,1)
+  )
+  if(feature.selection)genes.range <- c(genes.range.predictors, genes.range)
+
+  waRRior.machinelearning.geneticalgorithm.run(
+     chr.init = chr
+    ,genes.class = genes.class
+    ,genes.range = genes.range
+    ,train.data = train_hex
+    ,validation.data = valid_hex
+    ,test.data = test_hex
+    ,evaluate.function = evaluate_gbm
+    ,...
+  )  
 }
-
-
-chr <- list(
-   n.trees = 5
-  ,interaction.depth = 5
-  ,n.minobsinnode = 10
-  ,n.bins = 20
-  ,max.after.balance.size = 5
-  ,balance.classes = FALSE
-  ,distribution = "multinomial"
-  ,variables.include = sample(c(F,T), length(predictors), replace = T)
-  ,shrinkage = 0.1
-)
-
-genes.class <- list(
-   "integer"
-  ,"integer"
-  ,"integer"
-  ,"integer"
-  ,"integer"
-  ,"boolean"
-  ,"class"
-  ,"boolean.vector"
-  ,"float"
-)
-genes.range <- list(
-  c(1,100)
- ,c(1,100)
- ,c(1,100)
- ,c(1,100)
- ,c(1,100)
- ,c(F,T)
- ,c("multinomial","gaussian")
- ,c(F,T)
- ,c(0,1)
-)
-
-waRRior.machinelearning.geneticalgorithm.run(
-   chr.init = chr
-  ,genes.class = genes.class
-  ,genes.range = genes.range
-  ,train.data = train_hex
-  ,validation.data = valid_hex
-  ,test.data = test_hex
-  ,evaluate.function = evaluate_gbm
-  ,verbose = F
-  ,plot.logscale = F
-)
