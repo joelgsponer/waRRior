494b4caeee896a23f6e797692b53bda21bd8e6a9
Joel Gsponer <jgsponer@gmail.com>
2015-06-08 19:02:34 +0200

    Update waRRior.machinelearning.geneticalgorithm.run.R

--- machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.run.R
+++ machinelearning/geneticalgorithm/waRRior.machinelearning.geneticalgorithm.run.R
@@ -16,7 +16,7 @@ waRRior.machinelearning.geneticalgorithm.run <- function(
   ,function.id = "waRRior.machinelearning.geneticalgorithm.run" #Use this to identfy the function in error (or success messages if applicable) messages.
   ,verbose = F #Turn messages on and off
   ,simpleReturn = F
-  ,debug = T #Turn debug messages on and off
+  ,debug = F #Turn debug messages on and off
   ,plot.logscale = F
   ,save.population = T
   ,save.population.path = NA
@@ -60,7 +60,6 @@ waRRior.machinelearning.geneticalgorithm.run <- function(
       o <- order(scores)
       if(higher.score)o <- rev(order(scores))
       best.individual <- population[[o[1]]]
-      if(debug)print(best.individual@score)
       waRRior.snippets.verbose(paste("best score:",scores[o[1]]), verbose_ = verbose)
       mating_pool <- waRRior.machinelearning.geneticalgorithm.mating(o)
 
@@ -113,7 +112,7 @@ waRRior.machinelearning.geneticalgorithm.run <- function(
         )
       )  
     }
-    if(keep.best) old_population[[1]] <- best.individual
+    if(keep.best) population[[1]] <- best.individual
     for(i in seq(1, population.size)){
       s <- evaluate.function(population[[i]], train.data,validation.data,test.data,...)
       population[[i]]@score <- s
