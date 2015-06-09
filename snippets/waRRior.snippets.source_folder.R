aac37c2b582187e59136c1e45bc39b90423b1bad
Joel Gsponer <jgsponer@gmail.com>
2015-06-09 15:52:46 +0200

    Update waRRior.snippets.source_folder.R

--- snippets/waRRior.snippets.source_folder.R
+++ snippets/waRRior.snippets.source_folder.R
@@ -2,7 +2,7 @@ waRRior.snippets.source_folder <- function(path, lazy=F, verbose = T){
   waRRior.snippets.verbose(paste("loading files in folder:",path), verbose_ = verbose)
   for(file in list.files(path)){
     tryCatch({
-      cat("\t*Loading",file,"\t|\t")
+      cat("\t*Loading",file,"\t\t|\t")
       source(paste(path, file, sep = "/"))
       cat("OK.\n")
     }, error = function(e){
